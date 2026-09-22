224851ffc96afd501404886cd3efca92dbcbf402
Vagner Souza <juninfps0@gmail.com>
2026-09-21 21:40:39 -0300

    feat: Classe Pedido criada

--- src/main/java/dev/vagner/Commerce/domain/Pedido.java
+++ src/main/java/dev/vagner/Commerce/domain/Pedido.java
@@ -1,4 +1,33 @@
 package dev.vagner.Commerce.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "pedido")
 public class Pedido{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String status;
+    private Double totalAmount;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updateAt;
+
 }
